c20f85235071aaf3a4731c22fa5c38771860e439
TOMO <gozitomo@gmail.com>
2025-04-10 22:35:44 +0900

    管理者ページと発注者マイページを実装

---
+++ orders/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Order, OrderItem
+
+# Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'created_at', 'status')
+    inlines = [OrderItemInline]
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem)
