90c1aeb6095e65f5b4be11c78a03684769ce4a38
Hosono Tomo <gozitomo@gmail.com>
2025-04-11 17:44:32 +0900

    aplic2401作業分

--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.PositiveIntegerField()
     description = models.TextField(blank=True)
-    unit = models.CharField(max_length=50, default='個')
+    unit = models.CharField(max_length=50, default='kg')
 
     def __str__(self):
         return self.name
