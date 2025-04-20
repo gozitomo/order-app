bac228473b3dd73da3c014b3c2498d7efa71008a
TOMO <gozitomo@gmail.com>
2025-04-20 18:40:29 +0900

    create_superuser追加

---
+++ config/create_superuser.py
@@ -0,0 +1,23 @@
+import os
+import django
+
+#settingsモジュールのパス指定
+os.environ.setdefault("DJANGO_SETTING_MODULE", "config.settings")
+
+#Django セットアップ
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+#任意のsuperuser情報
+USERNAME = "admin"
+EMAIL = "admin@example.com"
+PASSWORD = "adminpass"
+
+if not User.objects.filter(username=USERNAME).exists():
+    User.objects.create_superuser(USERNAME, EMAIL, PASSWORD)
+    print("Superuser created successfully!")
+else:
+    pring("Superuser already exists.")
