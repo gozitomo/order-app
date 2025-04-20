4f64d9965722bd7eeabf7c4fa7965130bf3fa7e7
TOMO <gozitomo@gmail.com>
2025-04-20 17:02:46 +0900

    tailwindcss再構築

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@d@$jzccs9*izl4y$tc!qq2$^y$4@-82(*$&)d0^tx@drcs)$)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 # Application definition
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './orders/templates/**/*.html',
+    './users/templates/**/*.html',
     './templates/**/*.html',
-    './**/templates/**/*.html',
   ],
   theme: {
     extend: {},
