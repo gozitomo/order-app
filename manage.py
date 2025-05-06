19f898bdcfaf98c03ba5e8e302abe2d687be5b8f
TOMO <gozitomo@gmail.com>
2025-05-06 13:59:04 +0900

    envファイル読み込み設定

--- manage.py
+++ manage.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+import dotenv
 import os
 import sys
 
 
 def main():
+    dotenv.load_dotenv()
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
