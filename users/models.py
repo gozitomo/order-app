e1a6d88ef92f663e3b963d4c5153fe58fb2bb980
TOMO <gozitomo@gmail.com>
2025-05-29 22:56:00 +0900

    region_idにnull=Trueを追加

--- users/models.py
+++ users/models.py
@@ -14,7 +14,7 @@ class UserProfile(models.Model):
     company_name = models.CharField(max_length=255, blank=True)
     postal_code = models.CharField(max_length=255, default='000-0000')
     address = models.CharField(max_length=255)
-    region = models.ForeignKey('orders.ShippingRegion', related_name='users', on_delete=models.CASCADE)
+    region = models.ForeignKey('orders.ShippingRegion', related_name='users',null=True, on_delete=models.CASCADE)
     user_group = models.ForeignKey(UserGroup, null=True, blank=True, on_delete=models.CASCADE)
     def __str__(self):
         return f"{self.user.username}のプロフィール"
