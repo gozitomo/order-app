a69ef0c94854be4b2393acba966259bc2c055f8a
TOMO <gozitomo@gmail.com>
2025-05-31 21:31:22 +0900

    frommail変更 logout時toppage変更

--- config/settings.py
+++ config/settings.py
@@ -146,8 +146,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-LOGIN_REDIRECT_URL = '/orders/'
-LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 SECRET_KEY = config('SECRET_KEY')
 
