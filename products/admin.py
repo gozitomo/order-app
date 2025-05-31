e415e7c55a71d30f6b558a1e5a6edb87f4679db7
TOMO <gozitomo@gmail.com>
2025-05-31 14:47:52 +0900

    admin画面FruitKind表示変更

--- products/admin.py
+++ products/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from .models import FruitKind, ProductName, PriceTable, ProductDeliveryDate
 
 # Register your models here.
-admin.site.register(FruitKind)
+
+@admin.register(FruitKind)
+class FruitKindAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
 admin.site.register(ProductName)
 admin.site.register(PriceTable)
 admin.site.register(ProductDeliveryDate)
