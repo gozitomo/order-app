84d216c51c20cb222567c9b31c12fbf59ee605e4
TOMO <gozitomo@gmail.com>
2025-05-31 16:52:18 +0900

    modifyshippingregionmodel

--- orders/models.py
+++ orders/models.py
@@ -94,7 +94,7 @@ class ShippingRegion(models.Model):
     name = models.CharField(max_length=20)
 
     def __str__(self):
-        return f"{self.region}"
+        return f"{self.name}"
 
 
 class ShippingFeeRule(models.Model):
