41b4fc62ea9678b511f5d52917eaef0710bb4998
TOMO <gozitomo@gmail.com>
2025-05-31 20:03:13 +0900

    csvパスワードハッシュ化追加

--- orders/views.py
+++ orders/views.py
@@ -506,6 +506,10 @@ def upload_generic_csv(request):
                     lookup = {k: data[k] for k in unique_keys}
                     defaults = {k: v for k, v in data.items() if k not in unique_keys}
                     obj, created = model.objects.update_or_create(**lookup, defaults=defaults)
+                elif model.__name__ == 'User':
+                    obj = model(**data)
+                    obj.set_password(data['password']) #ハッシュ化
+                    obj.save()
                 else:
                     model.objects.create(**data)
                 created_count += 1
