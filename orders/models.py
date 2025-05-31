17197afe8148677752ce9d884dd2f546f629af50
TOMO <gozitomo@gmail.com>
2025-05-31 16:35:45 +0900

    region>>name

--- orders/models.py
+++ orders/models.py
@@ -91,7 +91,7 @@ class OrderItem(models.Model):
 
 
 class ShippingRegion(models.Model):
-    region = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
 
     def __str__(self):
         return f"{self.region}"
