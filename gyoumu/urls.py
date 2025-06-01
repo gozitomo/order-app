fd50f80c9e3c7d27e31fb5af06aac66b958039e5
TOMO <gozitomo@gmail.com>
2025-06-01 18:57:57 +0900

    add order_confirm

--- gyoumu/urls.py
+++ gyoumu/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('menu/',views.gyoumu_menu, name='gyoumu_menu'),
     path('menu/csv_uploader/',views.upload_generic_csv, name='csv_upload'),
     path('invoice/monthly/generate/',views.monthly_invoice_pdf, name='generate_monthly_invoice'),
+    path('menu/order_confirm/',views.order_confirm, name='order_confirm'),
+    path('menu/order_confirm/<int:order_id>',views.order_confirm, name='order_confirm'),
 ]
--- orders/models.py
+++ orders/models.py
@@ -77,7 +77,7 @@ class Order(models.Model):
 
     STATUS_CHOICES = [
         ('tentative', '仮注文'),
-        ('received', '確認済み'),
+        ('recieved', '確認済み'),
         ('preparing', '準備中'), #キャンセル不可
         ('shipped', '発送済'), #キャンセル不可
         ('canceled', 'キャンセル済'),
